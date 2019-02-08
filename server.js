0a3380b2823c820b03e12db02a24b5efdc367b55
Admir Osmanovic <admir.osmanovic@hhinc.eu>
2019-02-08 15:18:23 +0000

    prvi commit

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+var randomstring = require("randomstring");
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+var tekst = randomstring.generate();
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World! This is Admir! Random string: ' + tekst);
+});
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+
