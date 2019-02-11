17633f675df74bdf3712e1f565dd7a7f6ba64e61
admir93 <45433544+admir93@users.noreply.github.com>
2019-02-11 13:54:34 +0100

    Update docker-run-script.sh

--- docker-run-script.sh
+++ docker-run-script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-cd nodejs-docker
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker rmi admir93/nodejs-docker
-docker build -t admir93/nodejs-docker .
-docker run -p 8081:8080 -d --name nodejs-app1 admir93/nodejs-docker
-docker run -p 8082:8080 -d --name nodejs-app2 admir93/nodejs-docker
-docker run -p 8083:8080 -d --name nodejs-app3 admir93/nodejs-docker
-docker run -p 8084:8080 -d --name nodejs-app4 admir93/nodejs-docker
-docker run -p 8085:8080 -d --name nodejs-app5 admir93/nodejs-docker
-docker run -p 8086:8080 -d --name nodejs-app6 admir93/nodejs-docker
-
+docker rmi $(docker images -q)
+docker build -t admir93-docker .
+docker run -p 8081:8080 -d --name nodejs-app1 admir93-docker
+docker run -p 8082:8080 -d --name nodejs-app2 admir93-docker
+docker run -p 8083:8080 -d --name nodejs-app3 admir93-docker
+docker run -p 8084:8080 -d --name nodejs-app4 admir93-docker
+docker run -p 8085:8080 -d --name nodejs-app5 admir93-docker
+docker run -p 8086:8080 -d --name nodejs-app6 admir93-docker
